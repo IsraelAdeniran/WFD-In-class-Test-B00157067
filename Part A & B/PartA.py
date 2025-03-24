83c73f6c8be9608d555fe32a6ff37f68cea0e088
IsraelAdeniran <B00157067@mytudublin.ie>
2025-03-24 14:39:42 +0000

    Changes (A5 - A10)

--- Part A & B/PartA.py
+++ Part A & B/PartA.py
@@ -38,3 +38,47 @@ class Staff:
             self.address = value
 
 
+# A5 #
+class Manager(Staff):
+    def __init__(self, name, DoB, sex, staffID, address, department, level):
+        super().__init__(name, DoB, sex, staffID, address)
+        self.department = department
+        self.level = level
+
+    # A6 #
+    def show_profile(self):
+        self.display_details()
+        print("Department:", self.department)
+        print("Level:", self.level)
+
+    # A7 #
+    def set_department(self, value):
+        if isinstance(value, str):
+            self.department = value
+
+    def set_level(self, value):
+        if isinstance(value, int):
+            self.level = value
+
+
+# A8 #
+basic_staff = Staff("Michael Babatunde", "2000-10-10", "Male", 3001, "123 Africa Road")
+senior_manager = Manager("Jesutobiloba Robertson", "2024-11-14", "Male", 3002, "17 Benin Avenue", "Operations", 5)
+
+# A9 #
+print("\nStaff Member")
+basic_staff.display_details()
+
+print("\nManager Profile")
+senior_manager.show_profile()
+
+# A10 #
+print("\nUpdating Staff")
+basic_staff.set_name("Najeeb Adeniran")
+basic_staff.set_address("88 Libyan Crossing")
+basic_staff.display_details()
+
+print("\nUpdating Manager")
+senior_manager.set_department("Logistics")
+senior_manager.set_level(6)
+senior_manager.show_profile()
